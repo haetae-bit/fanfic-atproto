ca09180fae8ad1dfe878a81a3824ce54b59f5f2b
haetae <haetae@32bit.cafe>
2025-10-14 16:39:44 -0400

    optimize db call for chapter delete + refine atproto methods

--- src/actions/chapters/deleteChapter.ts
+++ src/actions/chapters/deleteChapter.ts
@@ -1,11 +1,35 @@
-import { defineAction } from "astro:actions";
+import { ActionError, defineAction } from "astro:actions";
+import { and, Chapters, db, eq, Users, Works } from "astro:db";
 
 export default defineAction({
   accept: "form",
   handler: async (_, context) => {
+    const loggedInUser = context.locals.loggedInUser;
+    if (!loggedInUser) {
+      throw new ActionError({
+        code: "UNAUTHORIZED",
+        message: "You're not logged in!",
+      });
+    }
+    
     const workSlug = context.params["workId"];
-    if (!workSlug) {
-      // there is no work or chapter!
+    const chapterSlug = context.params["chapterId"];
+    const [result] = await db.select()
+      .from(Chapters)
+      .fullJoin(Works, eq(Chapters.workId, Works.id))
+      .innerJoin(Users, eq(Works.author, loggedInUser.did))
+      .having(and(
+        eq(Works.slug, workSlug!),
+        eq(Works.author, loggedInUser.did)
+      ))
+      .where(eq(Chapters.slug, chapterSlug!))
+      .limit(1);
+    
+    if (!workSlug || !chapterSlug || !result) {
+      throw new ActionError({
+        code: "NOT_FOUND",
+        message: "This chapter was not found!",
+      });
     }
   },
 });
--- src/lib/atproto.ts
+++ src/lib/atproto.ts
@@ -2,10 +2,13 @@ import type { APIContext } from "astro";
 import { AtpBaseClient, AtUri } from "@atproto/api";
 import { DidResolver } from "@atproto/identity";
 import { AtProtoClient } from "./generated_client";
+import type { atProtoChapter, atProtoComment, atProtoWork, BskyPost, LeafletDoc } from "./types";
 
-export async function getAgent(locals: APIContext["locals"]) {
+export async function getAgent(locals?: APIContext["locals"]) {
   try {
-    const agent = new AtpBaseClient(locals.loggedInUser?.fetchHandler!);
+    const agent = new AtpBaseClient(
+      locals?.loggedInUser?.fetchHandler ?? { service: "api.bsky.social" }
+    );
     return agent;
   } catch (error) {
     // we don't need to return anything to make sure the site still functions for not logged in users?
@@ -24,8 +27,8 @@ export async function didToHandle(did: string) {
   }
 }
 
-export async function fetchBskyPost(uri: string) {
-  const { host, pathname } = new URL(uri);
+async function fetchBskyPost(url: string) {
+  const { host, pathname } = new URL(url);
   
   const bsky = [
     "blacksky.community",
@@ -44,14 +47,52 @@ export async function fetchBskyPost(uri: string) {
 
   const key = pathname.split("/")[-1];
   const handleOrDid = pathname.split("/")[1];
+  console.log(`rkey is ${key}\nhandle/did is ${handleOrDid}`);
   const atUri = AtUri.make(handleOrDid, "app.bsky.feed.post", key);
   return atUri;
 }
 
-export async function fetchLeaflet(uri: string) {
-
+async function fetchLeaflet(uri: string, did: string) {
+  const agent = await getAgent();
+  const atUri = new AtUri(uri);
+  const { collection, rkey } = atUri;
+  const record = await agent?.com.atproto.repo.getRecord({
+    rkey,
+    collection,
+    repo: did,
+  });
+  return record?.data;
 }
 
+export async function importChapter(option: "manual" | "bsky" | "leaflet", uri: string, did?: string) {
+  let chapterContent: BskyPost | LeafletDoc;
+  switch (option) {
+    case "bsky":
+      const bsky = await fetchBskyPost(uri);
+      console.log("bsky post: " + JSON.stringify(bsky));
+      chapterContent = {
+        $type: "fan.fics.work.chapter#bskyPost",
+        postRef: {
+          uri: bsky.toString(),
+          cid: ""
+        }
+      };
+      return chapterContent;
+    case "leaflet":
+      const leaflet = await fetchLeaflet(uri, did!);
+      console.log("leaflet: " + JSON.stringify(leaflet));
+      chapterContent = {
+        $type: "fan.fics.work.chapter#leafletDoc",
+        docRef: {
+          uri: leaflet!.uri,
+          cid: leaflet!.cid!,
+        }
+      };
+      return chapterContent;
+  }
+}
+
+// This is Slices-related code, which makes requests to Slices for fan.fics.*-specific lexicons
 const API_URL = import.meta.env.API_URL as string;
 const SLICE_URI = import.meta.env.SLICE_URI as string;
 const BEARER_TOKEN = import.meta.env.BEARER_TOKEN;
@@ -64,11 +105,14 @@ export const client = new AtProtoClient(
   // oauth | authprovider
 );
 
-export async function callSlices(
+// This is a very thin wrapper for the fetch requests to Slices
+async function callSlices(
   collection: "work" | "work.chapter" | "work.comment",
   endpoint: "createRecord" | "updateRecord" | "deleteRecord",
-  rkey: string,
-  record: any,
+  data: {
+    record?: atProtoWork | atProtoChapter | atProtoComment,
+    rkey?: string,
+  }
 ) {
   const url = `${XRPC}/${NAMESPACE}.${collection}.${endpoint}`;
   const params = {
@@ -86,8 +130,8 @@ export async function callSlices(
         const result = await fetch(url, {
           ...params,
           body: JSON.stringify({
-            record,
-            rkey,
+            record: data.record,
+            ...(endpoint === "updateRecord" && { rkey: data.rkey }),
             slice: SLICE_URI,
           }),
         });
@@ -101,19 +145,54 @@ export async function callSlices(
       try {
         const result = await fetch(url, {
           ...params,
-          body: JSON.stringify({ rkey }),
+          body: JSON.stringify({ rkey: data.rkey }),
         });
-        return result.ok;
+        return result;
       } catch (error) {
         console.error(error);
         throw new Error("Record failed to delete!");
       }
-    default:
-      break;
   }
 }
 
-export async function fetchSlices(query: string) {
+export async function createFanficWork(record: atProtoWork) {
+  return await callSlices("work", "createRecord", { record });
+}
+
+export async function updateFanficWork(record: atProtoWork, rkey: string) {
+  return await callSlices("work", "updateRecord", { record, rkey });
+}
+
+export async function deleteFanficWork(rkey: string) {
+  return await callSlices("work", "deleteRecord", { rkey });
+}
+
+export async function createFanficChapter(record: atProtoChapter) {
+  return await callSlices("work.chapter", "createRecord", { record });
+}
+
+export async function updateFanficChapter(record: atProtoChapter, rkey: string) {
+  return await callSlices("work.chapter", "updateRecord", { record, rkey });
+}
+
+export async function deleteFanficChapter(rkey:string) {
+  return await callSlices("work.chapter", "deleteRecord", { rkey });
+}
+
+export async function createFanficComment(record: atProtoComment) {
+  return await callSlices("work.comment", "createRecord", { record });
+}
+
+export async function updateFanficComment(record: atProtoComment, rkey: string) {
+  return await callSlices("work.comment", "updateRecord", { record, rkey });
+}
+
+export async function deleteFanficComment(rkey: string) {
+  return await callSlices("work.comment", "deleteRecord", { rkey });
+}
+
+// This is for finding specific fanfics through GraphQL, so this will be the SEARCH query
+export async function findFanfics(query: string) {
   const url = `${API_URL}/graphql?slice=${SLICE_URI}`;
   const result = await fetch(url, {
     method: "POST",
